aaa288358afc64f75e6e3aad392349bce5f2b588
Anton Stagge <antonstagge95@gmail.com>
2019-06-16 13:06:12 +0200

    Remove tornado and make scrollable

--- client/src/home/Home.tsx
+++ client/src/home/Home.tsx
@@ -1,333 +1,151 @@
-import * as React from 'react';
-import resumePic from 'src/resources/images/yosemity-falls.jpg';
-import aboutMePic from 'src/resources/images/vineyard.jpg';
-import contactPic from 'src/resources/images/typewriter.jpg';
-import computer from 'src/resources/images/desk.jpg';
-import logowhite from 'src/resources/images/logowhite.png';
-import BackgroundImage from 'src/shared/BackgroundImage';
-import MenuList from 'src/shared/MenuList';
-import Socials from './Socials';
-import NoStyleLink from 'src/shared/NoStyleLink';
-import { RouteComponentProps } from 'react-router-dom';
-import { debounce } from 'ts-debounce';
+import * as React from "react";
+import resumePic from "src/resources/images/yosemity-falls.jpg";
+import aboutMePic from "src/resources/images/vineyard.jpg";
+import contactPic from "src/resources/images/typewriter.jpg";
+import computer from "src/resources/images/desk.jpg";
+import logowhite from "src/resources/images/logowhite.png";
+import BackgroundImage from "src/shared/BackgroundImage";
+import MenuList from "src/shared/MenuList";
+import Socials from "./Socials";
+import NoStyleLink from "src/shared/NoStyleLink";
+import { RouteComponentProps } from "react-router-dom";
 
 export enum MenuChoice {
-    AboutMe = 0, 
-    Resume = 1,
-    Portfolio = 2,
-    Contact = 3,
+  AboutMe = 0,
+  Resume = 1,
+  Portfolio = 2,
+  Contact = 3
 }
 interface MenuItem {
-    title: string;
-    backgroundImage: string;
-    number: MenuChoice;
-    link: string;
+  title: string;
+  backgroundImage: string;
+  number: MenuChoice;
+  link: string;
 }
-export const animTime:number = 500;
+export const animTime: number = 500;
 export const numItems = 4;
 export const getMenuItem = (choice: MenuChoice): MenuItem => {
-    switch(choice) {
-        case MenuChoice.AboutMe: 
-            return {
-                title: 'ABOUT ME',
-                backgroundImage: aboutMePic,
-                number: choice,
-                link: 'aboutme'
-            }
-        case MenuChoice.Resume:
-            return {
-                title: 'RESUME',
-                backgroundImage: resumePic,
-                number: choice,
-                link: 'resume'
-            }
-        case MenuChoice.Portfolio:
-            return {
-                title: 'PORTFOLIO',
-                backgroundImage: computer,
-                number: choice,
-                link: 'portfolio'
-            }
-        case MenuChoice.Contact:
-            return {
-                title: 'CONTACT',
-                backgroundImage: contactPic,
-                number: choice,
-                link: 'contact'
-            }
-    }
-}
-
-enum ChangeDirection {
-    UP,
-    DOWN,
-}
-interface HomeState {
-    title: string;
-    active: MenuChoice;
-    changeTo: MenuChoice | -1;
-    changeDirection: ChangeDirection;
-    hover: boolean;
-    mousePos: {x: number, y: number}
-    tornado: boolean;
-    size: {x: number, y: number} | null;
+  switch (choice) {
+    case MenuChoice.AboutMe:
+      return {
+        title: "ABOUT ME",
+        backgroundImage: aboutMePic,
+        number: choice,
+        link: "aboutme"
+      };
+    case MenuChoice.Resume:
+      return {
+        title: "RESUME",
+        backgroundImage: resumePic,
+        number: choice,
+        link: "resume"
+      };
+    case MenuChoice.Portfolio:
+      return {
+        title: "PORTFOLIO",
+        backgroundImage: computer,
+        number: choice,
+        link: "portfolio"
+      };
+    case MenuChoice.Contact:
+      return {
+        title: "CONTACT",
+        backgroundImage: contactPic,
+        number: choice,
+        link: "contact"
+      };
+  }
 };
-class Home extends React.Component<RouteComponentProps, HomeState> {
-    private toggleTwice: boolean = false;
-    private firstTimeOutId = setTimeout(() => {/**/}, 0) as unknown as number;
-    private pStart = {x: 0, y:0};
-    private pStop = {x:0, y:0};
-
-    private cont: HTMLDivElement | null;
-    constructor(props: RouteComponentProps) {
-        super(props);
-        this.state = {
-            title: getMenuItem(this.props.location.state | 0).title,
-            active: this.props.location.state | 0,
-            changeTo: -1,
-            changeDirection: ChangeDirection.UP,
-            hover: false,
-            mousePos: {x: 0, y:0},
-            tornado: false,
-            size: null
-        };
-
-        this.handleMouseMove = debounce(this.handleMouseMove, 10);
-    }
-
-
-    public swipeStart = (e:any) => {
-        if (typeof e.targetTouches !== "undefined"){
-            const touch = e.targetTouches[0];
-            this.pStart.x = touch.screenX;
-            this.pStart.y = touch.screenY;
-        } else {
-            this.pStart.x = e.screenX;
-            this.pStart.y = e.screenY;
-        }
-    }
-
-    public swipeEnd = (e:any) => {
-        if (e.changedTouches !== "undefined"){
-            const touch = e.changedTouches[0];
-            this.pStop.x = touch.screenX;
-            this.pStop.y = touch.screenY;
-        } else {
-            this.pStop.x = e.screenX;
-            this.pStop.y = e.screenY;
-        }
-
-        this.swipeCheck();
-    }
-
-    public swipeCheck=() =>{
-        const changeY = this.pStart.y - this.pStop.y;
-        if (this.state.changeTo === -1) {
-            if (changeY < -50) {
-                this.changeUp()
-            } else if (changeY > 50) {
-                this.changeDown();
-            }
-        }
-    }
-
-    public componentDidMount() {
-        window.addEventListener('wheel', this.handleScroll, {passive: true});
-        window.addEventListener('keydown', this.handleKeyDown, {passive: true});
-        // window.addEventListener('touchstart', this.swipeStart, false);
-        // window.addEventListener('touchend', this.swipeEnd, false);
-        if (this.cont) {
-            this.setState({size: {x: this.cont.clientWidth, y: this.cont.clientHeight}})
-        }
-    }
-
-    public componentWillUnmount() {
-        window.removeEventListener('wheel', this.handleScroll);
-        window.removeEventListener('keydown', this.handleKeyDown);
-        // window.removeEventListener('touchstart', this.swipeStart, false);
-        // window.removeEventListener('touchend', this.swipeEnd, false);
-        const lastTimeOutId = setTimeout(() => {/**/}, 0) as unknown as number;
-        while (this.firstTimeOutId !== lastTimeOutId) {
-            clearTimeout(++this.firstTimeOutId);
-        }
-    }
-
-    public handleScroll = (e: WheelEvent) => {
-        if (this.state.changeTo === -1 && this.toggleTwice) {
-            this.toggleTwice = false;
-            if (e.deltaY > 0) {
-                this.changeDown();
-            } else {
-                this.changeUp();
-            }
-        } else {
-            this.state.changeTo === -1 ? this.toggleTwice = true : this.toggleTwice = false;
-        }
-    }
-
-    public handleKeyDown = (e: KeyboardEvent) => {
-        if (this.state.changeTo === -1) {
-            if (e.keyCode === 40) {
-                this.changeDown();
-            } else if (e.keyCode === 38) {
-                this.changeUp();
-            }
-        }
-    }
-
-    public changeDown = () => {
-        const nextChange = (this.state.active + 1) % numItems
-        this.setState({
-            changeTo: nextChange, 
-            changeDirection: ChangeDirection.DOWN
-        });
-        this.delayChange(nextChange);
-    }
-
-    public changeUp = () => {
-        const nextChange = (this.state.active + (numItems-1)) % numItems
-        this.setState({
-            changeTo: nextChange,
-            changeDirection: ChangeDirection.UP,
-        });
-        this.delayChange(nextChange);
-    }
-
-    public delayChange = (nextChange: MenuChoice) => {
-        setTimeout(() => this.setState({title: getMenuItem(nextChange).title}), animTime/2);
-        
-        setTimeout(() => {
-            this.setState({active: nextChange});
-            setTimeout(() => this.setState({changeTo: -1}), 1.5*animTime);
-        }, animTime);
-    }
-
-    public getClassName = (choice: MenuChoice) => {
-        if (this.state.changeTo !== -1 && this.state.active !== this.state.changeTo && (this.state.changeTo === choice || this.state.active === choice)) {
-            // getting swapped from or to
-            if (this.state.active === 0 && this.state.changeTo === numItems-1) {
-                // special case for from 0 to last
-                return (choice === 0 ? "bgAnimDownFirst" : "bgAnimDownLast");
-            } else if (this.state.active === numItems-1 && this.state.changeTo === 0) {
-                // special case for from last to 0
-                return (choice === 0 ? "bgAnimUpFirst" : "bgAnimUpLast");
-            }
-            return (this.state.changeDirection !== ChangeDirection.UP ? "bgAnimUp" : "bgAnimDown");
-        } else {
-            // active or not
-            return (this.state.active === choice ? "nothing" : "hidden")
-        }
-    }
-
-    public clickedChoice = (choice: MenuChoice) => {
-        this.props.history.push(getMenuItem(choice).link);
-    } 
-
-    public doubleClickedChoice = (choice: MenuChoice) => {
-        this.props.history.push(getMenuItem(choice).link);
-    }
-
-    public handleMouseMove = (x:number,y:number) => {
-        this.setState({
-            mousePos: {x, y}
-        });
-    }
-
-    public handleTouch = (choice: MenuChoice) => {
-        if (this.state.active + 1 === choice) {    
-            this.changeDown();	
-        } else if (this.state.active - 1 === choice) {	
-            this.changeUp();	
-        } else {	
-            this.setState({	
-                changeTo: choice,	
-                changeDirection: choice < this.state.active ? ChangeDirection.UP : ChangeDirection.DOWN,	
-            });	
-            this.delayChange(choice);	
-        }	
-    } 
-
-    public render() {
-        return <div
-            className="flex-1 max-h-full relative overflow-hidden font-header xs:h-middle sm:h-middle bg-black"
-            ref={cont => this.cont = cont}
-        >
-            {this.state.size !== null ? Array.from(Array(numItems).keys()).map(choice => {
-                return <BackgroundImage
-                    key={choice}
-                    backgroundImage={getMenuItem(choice).backgroundImage}
-                    className={this.getClassName(getMenuItem(choice).number)}
-                    mousePos={this.state.mousePos}
-                    interactive={true}
-                    showTornado={this.state.tornado}
-                    size={this.state.size || {x: 0, y: 0}}
-                />
-            }) : null}
-            <div className="absolute z-30 pin overflow-hidden"
-                onMouseMove={(e: React.MouseEvent<HTMLDivElement>) => this.handleMouseMove(e.pageX, e.pageY)}
-                onTouchMove={(e) => {if (e.touches.length !== 1) {return};this.handleMouseMove(e.touches[0].pageX, e.touches[0].pageY)}}
-            >
-                <div className="flex text-white h-full">
-                    <div className="flex-1 flex flex-col xs:pl-8 sm:pl-16">
-                        <div className="fadeIn flex-no-grow flex items-center overflow-visible pt-6">
-                            <img src={logowhite} alt=""
-                                className="xs:h-16 sm:h-32"
-                            />
-                            <div className="flex">
-                                <div className="xs:w-px xs:px-px sm:px-0 sm:w-1 xs:ml-1 sm:ml-2 bg-white"/>
-                                <div className="flex flex-col justify-end pl-px xs:text-xs sm:text-xl font-semibold">
-                                    <div className="flex-no-grow -mb-1 leading-normal">
-                                        ANTON 
-                                    </div>
-                                    <div className="flex-no-grow">
-                                        STAGGE
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <NoStyleLink 
-                            to={getMenuItem(this.state.active).link}
-                            className="flex-1 cursor-pointer flex flex-col justify-end xs:mb-8 sm:mb-16"
-                            onMouseEnter={() => this.setState({hover: true})}
-                            onMouseLeave={() => this.setState({hover: false})}
-                        >
-                             <div 
-                                className={"flex-no-grow xs:text-4xl sm:text-7xl font-bold flex " + 
-                                (this.state.changeTo !== -1 
-                                    ? "fadeOutIn"
-                                    : ""
-                                )
-                            }>
-                                <div className="flex-no-grow fadeIn ">
-                                    <div className="-mb-2">{this.state.title}</div>
-                                    <div className={"bg-white h-3 hoverBar " + (this.state.hover ? "w-full": "w-0") } />
-                                </div>
-                                <div className="flex-1"/>
-                            </div>
-                        </NoStyleLink>
-                    </div>
-                    <MenuList 
-                        className="flex-no-grow"
-                        onClick={this.clickedChoice}
-                        onTouch={this.handleTouch}
-                        titles={Array.from(Array(numItems).keys()).map(choice => getMenuItem(choice).title)}
-                        active={this.state.changeTo === -1 ? this.state.active : this.state.changeTo}
-                    />
-                </div>
-            </div>
-            <Socials 
-                className="absolute pin-t pin-r xs:pr-5 sm:pr-10 pt-10 xs:mt-1 sm:mt-0 z-30"
-            />
-            <div className="absolute pin-b pin-r xs:pr-5 sm:pr-10 pt-10 xs:mt-1 sm:mt-0 z-30 text-white flex">
-                <div className="leading-tight">Tornado&nbsp;</div>
-                <input type="checkbox" 
-                    checked={this.state.tornado} onChange={() => this.setState({tornado: !this.state.tornado})}
-                    className="mb-2"
-                />
-            </div>
-        </div>
-    }
+interface HomeState {
+  title: string;
+  active: MenuChoice;
+  hover: boolean;
 }
+class Home extends React.Component<RouteComponentProps, HomeState> {
+  constructor(props: RouteComponentProps) {
+    super(props);
+    this.state = {
+      title: getMenuItem(this.props.location.state | 0).title,
+      active: this.props.location.state | 0,
+      hover: false
+    };
+  }
+  public clickedChoice = (choice: MenuChoice) => {
+    this.props.history.push(getMenuItem(choice).link);
+  };
 
+  public doubleClickedChoice = (choice: MenuChoice) => {
+    this.props.history.push(getMenuItem(choice).link);
+  };
+
+  public render() {
+    return (
+      <div className="flex-1 max-h-full relative overflow-hidden font-header xs:h-middle sm:h-middle bg-black">
+        <div className="h-full overflow-y-scroll overflow-x-hidden">
+          {Array.from(Array(numItems).keys()).map(choice => {
+            return (
+              <BackgroundImage
+                key={choice}
+                backgroundImage={getMenuItem(choice).backgroundImage}
+              />
+            );
+          })}
+        </div>
+        <div className="absolute pin overflow-hidden">
+          <div className="flex text-white h-full">
+            <div className="flex-1 flex flex-col xs:pl-8 sm:pl-16">
+              <div className="fadeIn flex-no-grow flex items-center overflow-visible pt-6">
+                <img src={logowhite} alt="" className="xs:h-16 sm:h-32" />
+                <div className="flex">
+                  <div className="xs:w-px xs:px-px sm:px-0 sm:w-1 xs:ml-1 sm:ml-2 bg-white" />
+                  <div className="flex flex-col justify-end pl-px xs:text-xs sm:text-xl font-semibold">
+                    <div className="flex-no-grow -mb-1 leading-normal">
+                      ANTON
+                    </div>
+                    <div className="flex-no-grow">STAGGE</div>
+                  </div>
+                </div>
+              </div>
+              <NoStyleLink
+                to={getMenuItem(this.state.active).link}
+                className="flex-1 cursor-pointer flex flex-col justify-end xs:mb-8 sm:mb-16"
+                onMouseEnter={() => this.setState({ hover: true })}
+                onMouseLeave={() => this.setState({ hover: false })}
+              >
+                <div
+                  className={
+                    "flex-no-grow xs:text-4xl sm:text-7xl font-bold flex " +
+                    (this.state.active !== -1 ? "fadeOutIn" : "TODO")
+                  }
+                >
+                  <div className="flex-no-grow fadeIn ">
+                    <div className="-mb-2">{this.state.title}</div>
+                    <div
+                      className={
+                        "bg-white h-3 hoverBar " +
+                        (this.state.hover ? "w-full" : "w-0")
+                      }
+                    />
+                  </div>
+                  <div className="flex-1" />
+                </div>
+              </NoStyleLink>
+            </div>
+            <MenuList
+              className="flex-no-grow"
+              onClick={this.clickedChoice}
+              onTouch={coiche => console.log("TOUCH TODO")}
+              titles={Array.from(Array(numItems).keys()).map(
+                choice => getMenuItem(choice).title
+              )}
+              active={this.state.active}
+            />
+          </div>
+        </div>
+        <Socials className="absolute pin-t pin-r xs:pr-5 sm:pr-10 pt-10 xs:mt-1 sm:mt-0 z-30" />
+      </div>
+    );
+  }
+}
 
 export default Home;
--- client/src/shared/BackgroundImage.tsx
+++ client/src/shared/BackgroundImage.tsx
@@ -1,161 +1,40 @@
-import * as React from 'react';
-import fadeframe from 'src/resources/images/fade_frame.png';
+import * as React from "react";
+import fadeframe from "src/resources/images/fade_frame.png";
 interface BackgroundImageProps {
-    backgroundImage: string;
-    interactive: boolean;
-    showTornado?: boolean;
-    mousePos?: {x: number, y: number};
-    className?: string;
-    size?: {x: number, y:number};
+  backgroundImage: string;
+  className?: string;
 }
 
-interface BackgroundImageState {
-    imageDone: boolean;
-}
+class BackgroundImage extends React.Component<BackgroundImageProps> {
+  constructor(props: BackgroundImageProps) {
+    super(props);
+  }
 
-interface Vector {
-    x:number;
-    y:number;
-}
-
-class Particle {
-    public z: number;
-    public position: Vector;
-    public radius: number;
-    public offset: Vector;
-    public speed: number;
-    public color: string;
-    public oldCenter: Vector;
-
-    constructor(z:number) {
-        this.position = {x:0, y:0};
-        this.z = z;
-        this.radius = Math.random()*2 + 3 + 6 * z;
-        this.offset = {x: 0, y: 0};
-        this.speed = Math.random()*0.04 + 0.01;
-        this.oldCenter = {x:0, y:0};
-        this.color = "#" + (Math.random() * 0x000033 + 0xff9000).toString(16).substr(0, 6);
-    }
-}
-
-class BackgroundImage extends React.Component<BackgroundImageProps, BackgroundImageState> {
-    private canvas: HTMLCanvasElement;
-    private image: HTMLImageElement;
-    private ctx: CanvasRenderingContext2D;
-    private scaledImageWidth: number;
-
-    private mousePos: Vector;
-    private particles: Particle[];
-
-    constructor(props: BackgroundImageProps) {
-        super(props);
-        this.state = {
-            imageDone: false,
-        }
-    }
-
-    public componentDidMount() {
-        if (!this.props.interactive) {this.setState({imageDone: true});return;}
-        const tempctx = this.canvas.getContext("2d");
-        if (tempctx === null) { return;}
-        this.ctx = tempctx;
-        this.resizeCanvas();
-        window.onresize = this.resizeCanvas;
-
-        this.image = new Image();
-        this.image.onload = () => {
-            this.ctx.imageSmoothingEnabled = false;
-            this.scaledImageWidth = Math.max(this.canvas.width, (this.canvas.height / this.image.height) * this.image.width);
-            this.ctx.clearRect(0,0, this.canvas.width, this.canvas.height);
-            this.ctx.drawImage(this.image, 0,0, this.scaledImageWidth, this.canvas.height);
-            this.setState({imageDone: true});
-        }
-        this.image.src= this.props.backgroundImage;
-        this.mousePos = {x: this.canvas.width/2, y: this.canvas.height/2};
-        this.particles = []
-
-        const NUM_PARTICLES = 60;
-        for (let i = 0; i< NUM_PARTICLES; ++i) {
-            const p = new Particle(i/NUM_PARTICLES);
-            this.particles.push(p);
-        }
-        this.ctx.globalCompositeOperation='difference';
-        this.animate();
-    }
-
-    public componentWillReceiveProps(nextProps: BackgroundImageProps) {
-        if (!this.props.interactive || !nextProps.mousePos) {return;}
-        const mouseX = nextProps.mousePos.x - this.canvas.getBoundingClientRect().left;
-        const mouseY = nextProps.mousePos.y - this.canvas.getBoundingClientRect().top;
-        this.mousePos = {x: mouseX, y: mouseY};
-    }
-
-    public resizeCanvas = () => {
-        if (this.props.size) {
-            const w = this.props.size.x;
-            const h = this.props.size.y;
-            this.canvas.width = w// - (w < 576 ? 26 : 50);
-            this.canvas.height = h// - (w < 576 ? 26 : 50) + 2;
-        }
-    }
-
-    public line = (t: number) => {
-        return {x: (1-t) * this.canvas.width/2 + t * this.mousePos.x, y: (1-t) * this.canvas.height/2 + t * this.mousePos.y}
-    }
-
-    public animate = () => {
-        requestAnimationFrame(this.animate);
-        if (this.props.className === "hidden") {return}
-        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-        this.ctx.drawImage(this.image, 0,0, this.scaledImageWidth, this.canvas.height);
-        
-        if (!this.props.showTornado) {return}
-        const RADIUS = 100;
-        for (let i = 0, len = this.particles.length; i < len; ++i) {
-            const particle = this.particles[i]
-            
-            particle.offset.x += particle.speed;
-            particle.offset.y += particle.speed;
-            
-            const newCenter = this.line(particle.z);
-
-            const shiftX = (newCenter.x - particle.oldCenter.x) * particle.speed;
-            const shiftY = (newCenter.y - particle.oldCenter.y) * particle.speed;
-            
-            this.ctx.fillStyle = particle.color;
-
-            particle.position.x = particle.oldCenter.x + shiftX + Math.cos(particle.offset.x) * (RADIUS + particle.z * 50);
-            particle.position.y = particle.oldCenter.y + shiftY + Math.sin(particle.offset.y) * (RADIUS + particle.z * 50);
-            particle.oldCenter = {x: particle.oldCenter.x + shiftX, y: particle.oldCenter.y + shiftY};
-            
-            this.ctx.beginPath();
-            this.ctx.arc(particle.position.x, particle.position.y, particle.radius, 0, 2*Math.PI);
-            this.ctx.fill();
-        }
-    }
-
-    public render() {
-        const {interactive, className, backgroundImage} = this.props;
-        console.log(className, backgroundImage)
-        return (<div className={className + " relative "}>
-            <img 
-                src={fadeframe} alt="fadeframe"
-                className="absolute z-20 w-full xs:h-middle sm:h-middle "
-                style={{
-                    opacity: 0.3,
-                    pointerEvents: 'none'
-                }}
-            />
-            <img 
-                src={backgroundImage} alt="background"
-                className={"min-w-full xs:h-middle sm:h-middle " + (className === "nothing" && interactive && this.state.imageDone ? "hidden" : "")}
-                style={{
-                    maxWidth: 'unset',
-                }}
-            />
-            {interactive ? <canvas ref={c => c !== null ? this.canvas = c : null} className={"absolute pin z-10 " + ""}/> : null}
-        </div>)
-    }
+  public render() {
+    const { className, backgroundImage } = this.props;
+    return (
+      <div className={className + " relative "}>
+        <div className="absolute z-10 w-full xs:h-middle sm:h-middle pointer-events-none" />
+        <img
+          src={fadeframe}
+          alt="fadeframe"
+          className="absolute w-full xs:h-middle sm:h-middle "
+          style={{
+            opacity: 0.3,
+            pointerEvents: "none"
+          }}
+        />
+        <img
+          src={backgroundImage}
+          alt="background"
+          className={"min-w-full xs:h-middle sm:h-middle "}
+          style={{
+            maxWidth: "unset"
+          }}
+        />
+      </div>
+    );
+  }
 }
 
 export default BackgroundImage;
