066da2b848990ce60d476c1954e3ab0fa227114c
Anton Stagge <antonstagge95@gmail.com>
2020-08-11 17:15:23 +0200

    Update poll for emil

--- poll_fp.py
+++ poll_fp.py
@@ -5,9 +5,10 @@ from email.mime.text import MIMEText
 import smtplib
 import config
 
-def send_mail(new_times):
+def send_mail(subject, new_times):
+    print("Sending mail...")
     msg = MIMEMultipart()
-    msg['Subject'] = "Email from from website server."
+    msg['Subject'] = subject
     text = "New times available:\n\n"
     for t in new_times:
         text += t + "\n"
@@ -27,41 +28,71 @@ def send_mail(new_times):
         print(str(e))
         return False
 
+def get_payload(ssn, locationId, typeId):
+    return ('{"bookingSession":{"socialSecurityNumber":"%s","licenceId":4,"bookingModeId":0,"ignoreDebt":false,"ignoreBookingHindrance":false,"excludeExaminationCategories":[],"rescheduleTypeId":0,"paymentIsActive":false,"paymentReference":null,"paymentUrl":null},"occasionBundleQuery":{"startDate":"2020-08-11T22:00:00.000Z","locationId":%d,"languageId":13,"vehicleTypeId":1,"tachographTypeId":1,"occasionChoiceId":1,"examinationTypeId":%d}}' % (ssn, locationId, typeId))
 
 my_mail = config.MAIL_ADDR
 pw = config.MAIL_PWD
+url = 'https://fp.trafikverket.se/Boka/occasion-bundles'
+headers = {'content-type': 'application/json', 'Accept-Charset': 'UTF-8'}
 
 TEST = False
 
-url = 'https://fp.trafikverket.se/Boka/occasion-bundles'
-# theory
-# payload = '{"bookingSession":{"socialSecurityNumber":"19950102-0078","licenceId":4,"bookingModeId":0,"ignoreDebt":false,"ignoreBookingHindrance":false,"excludeExaminationCategories":[],"rescheduleTypeId":0,"paymentIsActive":false,"paymentReference":null,"paymentUrl":null},"occasionBundleQuery":{"startDate":"2020-04-04T22:00:00.000Z","locationId":1000140,"languageId":13,"vehicleTypeId":1,"tachographTypeId":1,"occasionChoiceId":1,"examinationTypeId":2}}'
+sthlm = 1000140
+gillinge_1 = 1000301
+gillinge_2 = 1000302
+theory = 2
+driving = 10
+emil_ssn = '19950803-1532'
+
+current_theory_time = datetime.strptime("2020-09-21T18:00:00", "%Y-%m-%dT%H:%M:%S").date()
+current_driving_time = datetime.strptime("2020-12-01T18:00:00", "%Y-%m-%dT%H:%M:%S").date()
+
+# Theory
+r = requests.post(url, data=get_payload(emil_ssn, sthlm, theory), headers=headers)
+data = r.json()['data']
+print("Theory status codes:")
+print("Status code: %s" % (r.json()['status']))
+new_times = []
+if len(data) == 0:
+    print("No theory times found...")
+else:
+    print("Earliest found: %s" % (data[0]['occasions'][0]['duration']['start']))
+    for day in data:
+        occasions = day['occasions']
+        for occ in occasions:
+            start = occ['duration']['start'][:-6]
+            d = datetime.strptime(start, "%Y-%m-%dT%H:%M:%S").date()
+            if d < current_theory_time or TEST:
+                print(d)
+                new_times.append(start)
+
+    if len(new_times) != 0:
+        send_mail("New theory times in sthlm!", new_times)
+
 # driving
-payload = '{"bookingSession":{"socialSecurityNumber":"19950102-0078","licenceId":4,"bookingModeId":0,"ignoreDebt":false,"ignoreBookingHindrance":false,"excludeExaminationCategories":[],"rescheduleTypeId":0,"paymentIsActive":false,"paymentReference":null,"paymentUrl":null},"occasionBundleQuery":{"startDate":"2020-04-08T22:00:00.000Z","locationId":1000301,"languageId":13,"vehicleTypeId":1,"tachographTypeId":1,"occasionChoiceId":1,"examinationTypeId":10}}'
-payload2 = '{"bookingSession":{"socialSecurityNumber":"19950102-0078","licenceId":4,"bookingModeId":0,"ignoreDebt":false,"ignoreBookingHindrance":false,"excludeExaminationCategories":[],"rescheduleTypeId":0,"paymentIsActive":false,"paymentReference":null,"paymentUrl":null},"occasionBundleQuery":{"startDate":"2020-04-08T22:00:00.000Z","locationId":1000302,"languageId":13,"vehicleTypeId":1,"tachographTypeId":1,"occasionChoiceId":1,"examinationTypeId":10}}'
-headers = {'content-type': 'application/json', 'Accept-Charset': 'UTF-8'}
-r = requests.post(url, data=payload, headers=headers)
-r2 = requests.post(url, data=payload2, headers=headers)
+r = requests.post(url, data=get_payload(emil_ssn, gillinge_1, driving), headers=headers)
+r2 = requests.post(url, data=get_payload(emil_ssn, gillinge_2, driving), headers=headers)
 data = r.json()['data']
 m_data = r2.json()['data']
 for d in m_data:
     data.append(d)
+print("driving status codes:")
 print("Status code: %s" % (r.json()['status']))
 print("Status code2: %s" % (r2.json()['status']))
-my_current_time = datetime.strptime("2020-09-13T14:00:00", "%Y-%m-%dT%H:%M:%S").date()
 new_times = []
 if len(data) == 0:
-    print("No times found...")
-    exit()
-print("Earliest found: %s" % (data[0]['occasions'][0]['duration']['start']))
-for day in data:
-    occasions = day['occasions']
-    for occ in occasions:
-        start = occ['duration']['start'][:-6]
-        d = datetime.strptime(start, "%Y-%m-%dT%H:%M:%S").date()
-        if d < my_current_time or TEST:
-            print(d)
-            new_times.append(start)
+    print("No driving times found...")
+else:
+    print("Earliest found: %s" % (data[0]['occasions'][0]['duration']['start']))
+    for day in data:
+        occasions = day['occasions']
+        for occ in occasions:
+            start = occ['duration']['start'][:-6]
+            d = datetime.strptime(start, "%Y-%m-%dT%H:%M:%S").date()
+            if d < current_driving_time or TEST:
+                print(d)
+                new_times.append(start)
 
-if len(new_times) != 0:
-    send_mail(new_times)
+    if len(new_times) != 0:
+        send_mail("New driving times at gillinge!", new_times)
