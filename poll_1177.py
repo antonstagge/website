a91d6c833d7f449e7e6f01079b31d82ef9b471e1
Anton Stagge <antonstagge95@gmail.com>
2021-04-25 17:13:09 +0200

    Also mail diff

--- poll_1177.py
+++ poll_1177.py
@@ -9,11 +9,14 @@ import codecs
 import hashlib
 import re
 
-def send_mail():
+def send_mail(output):
     print("Sending mail...")
     msg = MIMEMultipart()
     msg['Subject'] = "New vaccine times available!!"
-    msg.attach(MIMEText(url, 'plain'))
+
+    text = url + "\n\n" + output
+
+    msg.attach(MIMEText(text, 'plain'))
     text_msg = msg.as_string()
 
     server = smtplib.SMTP('smtp.gmail.com', 587)
@@ -30,8 +33,9 @@ def send_mail():
         print(str(e))
         return False
 
+
 my_mail = config.MAIL_ADDR
-to_mails = [my_mail]
+to_mails = [my_mail, 'cassanoalexandra@gmail.com']
 pw = config.MAIL_PWD
 url = 'https://www.1177.se/Stockholm/sjukdomar--besvar/lungor-och-luftvagar/inflammation-och-infektion-ilungor-och-luftror/om-covid-19--coronavirus/om-vaccin-mot-covid-19/boka-tid-for-vaccination-mot-covid-19-i-stockholms-lan/'
 headers = {'content-type': 'application/json', 'Accept-Charset': 'UTF-8'}
@@ -46,6 +50,7 @@ if not os.path.exists(path + file_name_1) or not os.path.exists(path + file_name
     with open(path + file_name_2, 'w'): pass
 
 new = requests.get(url, headers=headers).text
+new = re.sub(r"(<[a-z]+)\s[^<]*>", r"\1/>\n", new)
 
 writer_new = codecs.open(path + file_name_2, 'w', "utf-8")
 writer_new.write(new)
@@ -55,26 +60,28 @@ reader_current = codecs.open(path + file_name_1, 'r', "utf-8")
 current = reader_current.read()
 reader_current.close()
 
-current = re.sub(r"(<[a-z]+)\s[^<]*>", r"\1/>", current)
-
 reader_new = codecs.open(path + file_name_2, 'r', "utf-8")
 new = reader_new.read()
 reader_new.close()
-
-new = re.sub(r"(<[a-z]+)\s[^<]*>", r"\1/>", new)
 
 hash_current = hashlib.md5(current.encode("utf-8")).hexdigest()
 hash_new = hashlib.md5(new.encode("utf-8")).hexdigest()
 print(hash_current)
 print(hash_new)
 
-writer_next = codecs.open(path + file_name_1, 'w', "utf-8")
-writer_next.write(new)
-writer_next.close()
-
 if current == "":
     print("First run")
     exit()
 if hash_current != hash_new:
     print("Sending")
-    send_mail()
+
+    bashCommand = "diff %s %s" % (path + file_name_1, path + file_name_2)
+    cmd = bashCommand.split()
+    import subprocess
+    process = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+    output, error = process.communicate()
+    send_mail(output.decode("utf-8", "ignore"))
+
+writer_next = codecs.open(path + file_name_1, 'w', "utf-8")
+writer_next.write(new)
+writer_next.close()
